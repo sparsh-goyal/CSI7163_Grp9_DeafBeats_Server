63c7e0d77b64cb37eed04aa2e7671bd47605c890
Sparsh Goyal <sparshg99.sg@gmail.com>
2023-03-08 01:23:42 -0500

    Client Config

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
+  //luciana
   // apiKey: "AIzaSyAwP4gllGuO5pCmUwycUKebgIASVtEv9oc",
   // authDomain: "uploadingfile-c6f8a.firebaseapp.com",
   // projectId: "uploadingfile-c6f8a",
