2a8a7b909dfc451aa4dc858b2280b51604f4e8da
Sparsh Goyal <sparshg99.sg@gmail.com>
2023-03-08 02:03:01 -0500

    cleanup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import WebcamStreamCapture from "./Client";
+import Teach from "./Teach";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <WebcamStreamCapture />
+    <Teach />
   </React.StrictMode>
 );
 
